7329b0622c67649cd7bb5b988c692a103a50ed9e
rubbitang-yoyoyo <rubbitang@gmail.com>
2026-04-22 10:34:20 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -309,8 +309,8 @@ function formatDate(date) {
 
 function formatNumber(num) {
   if (num >= 1000000) return (num / 1000000).toFixed(1) + 'M';
-  if (num >= 1000) return (num / 1000).toFixed(1) + 'K';
-  return String(num);
+  if (num >= 10000) return (num / 1000).toFixed(1) + 'K';
+  return num.toLocaleString();
 }
 
 function formatCurrency(num) {
